8a730a3dd2d01e5f03b2a810b16f01d0e6df0e3d
ohanke <steamoha@wp.pl>
2021-09-11 12:06:48 +0200

    komentarz

---
+++ src/com/company/Zad1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Zad1 {
+    public static void main(String[] args) {
+        displayWordUsingDecimalFormat();
+    }
+
+    private static void displayWordUsingDecimalFormat(){
+        char s = 0b01010011; //binary 83
+        char d = 68;
+        char a = 0x41;  //hex 65
+
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+}
---
+++ src/com/company/Zad2.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Zad2 {
+    private static final float PI = 3.14F;
+
+    public static void main(String[] args) {
+        float radius = getDiameterFromUser();
+        float circumferenceOfACircle = calculateCircumferenceOfACircle(radius);
+        float areaOfACircle = calculateAreaOfACircle(radius);
+
+        System.out.println("Circumference Of A Circle: " + circumferenceOfACircle);
+        System.out.printf("Circumference Of A Circle: %.2f\n", circumferenceOfACircle); //round 2
+        System.out.format("Circumference Of A Circle: %.2f\n", circumferenceOfACircle);
+
+        System.out.println("Area Of A Circle: " + areaOfACircle);
+        System.out.printf("Area Of A Circle: %.2f\n", areaOfACircle);
+        System.out.format("Area Of A Circle: %.2f\n", areaOfACircle);
+
+    }
+
+    private static float calculateAreaOfACircle(float radius) {
+        return PI * (radius/2) * (radius/2);
+    }
+
+    private static float calculateCircumferenceOfACircle(float radius) {
+        return 2*PI*(radius/2);
+    }
+
+    private static float getDiameterFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        float diameter = 0;
+        try {
+            System.out.print("Please insert diameter: ");
+            diameter = scanner.nextFloat();
+        } catch (InputMismatchException exception){
+            System.out.println("Insert only digits!");
+            System.exit(1);
+        }
+        return diameter;
+    }
+
+
+}
