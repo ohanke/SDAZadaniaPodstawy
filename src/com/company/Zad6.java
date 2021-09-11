c5c9cda39430f24156c3babe03ec5f4bc8a4eb4a
ohanke <steamoha@wp.pl>
2021-09-11 13:27:26 +0200

    zadanie 6

---
+++ src/com/company/Zad6.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Zad6 {
+    public static void main(String[] args) {
+        int multiplier = 6;
+        int minMultiplicand = 2;
+        int maxMultiplicand = 5;
+
+        printMultiplicationTable(multiplier, minMultiplicand, maxMultiplicand);
+    }
+
+    private static void printMultiplicationTable(int multiplier, int minMultiplicand, int maxMultiplicand) {
+        for(;minMultiplicand <= maxMultiplicand; minMultiplicand++){
+            int result = multiplier;
+            result *= minMultiplicand;
+            System.out.println(multiplier + " x " + minMultiplicand + " = " + result);
+        }
+    }
+}
---
+++ src/com/company/Zad7.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Zad7 {
+    public static void main(String[] args) {
+        System.out.print("Enter the first digit");
+        Scanner scanner = new Scanner(System.in);
+        float firstDigit = scanner.nextFloat();
+        System.out.print("\nEnter operator: ( + - / * )");
+        //char operator = scanner.next
+        System.out.print("\nEnter the first digit");
+        float secondDigit = scanner.nextFloat();
+
+        System.out.println();
+    }
+}
